b021fc3bc2a1c717176c9c3e1ce566ac9bb6aab7
GreenCord <steve@nevizen.com>
2018-01-21 12:24:26 -0500

    Collecting user's order and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,8 +30,7 @@ function start() {
 		for (var i = 0; i < res.length; i++) {
 			depts.push(res[i].department_id + ': ' + res[i].department_name);
 		}
-		var quitID = res.length+1;
-		depts.push(quitID + ': Quit');
+		depts.push('Q: Quit Shopping');
 		// console.log(depts);
 
 		// inquirer message - choose department or quit
@@ -45,16 +44,89 @@ function start() {
 			var id = arr[0];
 			console.log('Dept chosen:',id);
 
+			switch (id) {
+				case '0':
+					// pass no where into query
+					placeOrder(';');
+				break;
+				case 'Q':
+					console.log('Thank you for shopping bamazon. Goodbye!');
+					connection.end();
+				break;
+				default: 
+					// pass id into query
+					placeOrder(' WHERE departments.department_id = ' + id + ';');
+			}
 		});
-	// promise returned - list products from department & go to placeOrder
+	// promise returned - go to placeOrder
 	});
 }
 
-function placeOrder() {
+function placeOrder(where) {
 	console.log('Attempting to place order...');
-	// inquirer message - get user's input for item_id and quantity to purchase
+	// list products
+	var queryall  = 'SELECT product_list.product_id, quantity, product_name, product_desc, price, departments.department_name ';
+			queryall += 'FROM product_list ';
+			queryall += 'INNER JOIN products ON (product_list.product_id = products.product_id) ';
+			queryall += 'LEFT JOIN departments ON (products.department_id = departments.department_id)';
+			queryall += where;
+	console.log('Query:',queryall);
+	connection.query(queryall, (err,res) => {
+		if (err) throw err;
+		console.log('ID | Price | Name');
+		console.log('___________________________________________________________');
+		for (var i = 0; i < res.length; i++) {
+			var line = '';
+			if (res[i].product_id <= 9) {
+				line += ' ' + res[i].product_id;
+			} else {
+				line += res[i].product_id;
+			}
+			line += ' | ';
+			if (res[i].price < 10) {
+				line += '$ ' + res[i].price.toFixed(2);
+			} else {
+				line += '$' + res[i].price.toFixed(2);
+			}
+			line += ' | ' + res[i].product_name;
+			console.log(line);
+		}
+		inquirer
+		.prompt([
+			{
+				name: 'item',
+				type: 'input',
+				message: 'Enter the ID of the product you want to buy:',
+				validate: function(value){
+					var isok = /^\d*$/.test(value);
+					if (isok) {
+						return true;
+					}
+					return 'Please enter the ID number of the product you want to purchase.';
+				}
+			},
+			{
+				name: 'quantity',
+				type: 'input',
+				message: 'Enter the amount you wish to buy:',
+				validate: function(value){
+					var isok = /^\d*$/.test(value);
+					if (isok) {
+						return true;
+					}
+					return 'Please enter a numeric value of the quantity you want to buy.';
+				}
+			}
+		])
+		.then((ans)=>{
+			console.log('You want to buy ' + ans.quantity + ' of Item No. ' + ans.item + '.');
+			start();
+		});
 	// promise returned - check database quantities, if insufficient - error to user, else update db and display order total
 	// go back to start
+	});
+	// inquirer message - get user's input for item_id and quantity to purchase
+
 }
 
 // function start() {
