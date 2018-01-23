fc6893165973de18299cf8ac59fc392d8e7a7af1
GreenCord <steve@nevizen.com>
2018-01-23 17:21:22 -0500

    Forgot departments seed

---
+++ departmentsSeed.sql
@@ -0,0 +1,10 @@
+USE bamazon;
+INSERT INTO departments (department_name, department_desc)
+VALUES
+('Bakery','We have welcomed our carbohydrate overlords.'),
+('Dairy','Locally sourced dairy and organic, pesticide-free, processed cheese foods.'),
+('Deli','Do you want nitrites? This is how you get nitrites. Slice meats and cheeses as far as the eye can see.'),
+('Frozen','Appetizers, desserts, and TV Dinners, because cooking is hard.'),
+('Grocery','All the processed food and boxed stuff you are supposed to skip, but tastes too good.'),
+('Meat','MEAAAAAAAAAATS. Caution: Eating undercooked meats could cause foodborne illnesses. Caution: If you eat your steaks well done, you are just dead inside.'),
+('Produce','We got your seven servings of fruit and vegetable right here, buddy.');
